3f4838f803c34a6adad152e4890c647359a8d1e5
Arim <dkflarnjs11@gmail.com>
2023-06-19 11:41:46 +0900

    Initial Commit

---
+++ Lifesum/LifesumApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifesumApp.swift
+//  Lifesum
+//
+//  Created by 권아림 on 2023/06/19.
+//
+
+import SwiftUI
+
+@main
+struct LifesumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
